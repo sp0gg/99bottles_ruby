81d2855bbfc83cfdeb234602b9375f0bf8946610
sp0gg <joe.nelson@runbox.com>
2021-05-05 21:49:02 -0400

    extract bottle class

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Bottles # committed after 30 minutes
+class Bottles
   def song
     verses(99, 0)
   end
@@ -10,44 +10,79 @@ class Bottles # committed after 30 minutes
   end
 
   def verse(count)
-    "#{main_phrase(count)} of beer on the wall, " \
-      "#{main_phrase(count, capitalize: false)} of beer.\n" \
-      "#{final_phrase(count)} of beer on the wall.\n"
+    bottle = Bottle.new(count)
+    "#{bottle.main_phrase} of beer on the wall, " \
+      "#{bottle.main_phrase(capitalize: false)} of beer.\n" \
+      "#{bottle.final_phrase} of beer on the wall.\n"
+  end
+end
+
+class Bottle
+  def initialize(number)
+    @number = number
   end
 
-  def main_phrase(count, **args)
-    "#{bottles_starting_word(count, args)} #{bottle_word(count)}"
+  def main_phrase(**args)
+    "#{bottles_starting_word(args)} #{bottle_word}"
   end
 
-  def final_phrase(count)
-    return 'Go to the store and buy some more, 99 bottles' if count.zero?
+  def final_phrase
+    return 'Go to the store and buy some more, 99 bottles' if number.zero?
 
-    "Take #{bottles_to_take_word(count)} down and pass it around, " \
-      "#{bottles_remaining_word(count)} #{bottle_word(count - 1)}"
+    "Take #{bottles_to_take_word} down and pass it around, " \
+      "#{quantity_remaining_word} #{bottles_remaining_word}"
   end
 
-  def bottles_starting_word(count, capitalize: true)
+  def bottles_starting_word(capitalize: true)
     word = capitalize ? 'No more' : 'no more'
-    return word if count.zero?
+    return word if number.zero?
 
-    count
+    number
   end
 
-  def bottle_word(count)
-    return 'bottle' if count == 1
+  def bottle_word
+    return 'bottle' if (number) == 1
 
     'bottles'
   end
 
-  def bottles_remaining_word(count)
-    return 'no more' if count == 1
+  def bottles_remaining_word
+    return 'bottle' if (number - 1) == 1
 
-    count - 1
+    'bottles'
   end
 
-  def bottles_to_take_word(count)
-    return 'it' if count == 1
+  def quantity_remaining_word
+    return 'no more' if number == 1
+
+    number - 1
+  end
+
+  def bottles_to_take_word
+    return 'it' if number == 1
 
     'one'
   end
+
+  private
+
+  attr_reader :number
 end
+
+# class Wall
+#   def initialize(quantity)
+#     @bottles_remaining = quantity
+#   end
+#
+#   def take_bottle
+#     bottle = Bottle.new(bottles_remaining)
+#     @bottles_remaining -= 1
+#     bottle
+#   end
+#
+#   attr_reader :bottles_remaining
+#
+#   private
+#
+#   attr_writer :bottles_remaining
+# end
